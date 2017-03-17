87d704df67be0d6cde78b97c5d13b9cf012bbb50
Tricia Wise <Visciabia@gmail.com>
2017-03-16 21:16:57 -0600

    HTML changes
    Added working links to pages in nav bar and created blnak pages for
    placeholders. Added jQuery script and "back to top" button

--- untitled/info.js
+++ untitled/info.js
@@ -1,4 +1,18 @@
-$("button").on("click", function(){
+$(document).ready(function() {
+    var offset = 220;
+    var duration = 500;
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > offset) {
+            $('.back-to-top').fadeIn(duration);
+        } else {
+            $('.back-to-top').fadeOut(duration);
+        }
+    });
 
+    $('.back-to-top').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+    })
 });
 
